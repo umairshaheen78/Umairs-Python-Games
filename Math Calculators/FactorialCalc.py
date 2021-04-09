f0d33a7b56707a4aeef5f23858f0d7422eee35ef
Umair Shaheen <>
2021-04-09 22:26:30 +0000

    Initial commit

--- Math Calculators/FactorialCalc.py
+++ Math Calculators/FactorialCalc.py
@@ -1,6 +1,6 @@
 def factorial(n):
     if n == 0:
-        return 1
+        return 1 and print ("0 doesn't really have a factorial, so that's why it's marked to be 1. The answer also might be marked as None.")
     else:
         return n * factorial(n-1)
 print ("Welcome to the Factorial Calculator!")
